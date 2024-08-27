5396dbf4ae31009a2e6998fe4ca29a459354583f
Roland Thompson <rolanditaru@gmail.com>
2024-08-27 15:07:46 +1000

    Update code to download model files from artifactory

--- spacy/about.py
+++ spacy/about.py
@@ -1,5 +1,5 @@
 # fmt: off
 __title__ = "spacy"
 __version__ = "3.7.6"
-__download_url__ = "https://github.com/explosion/spacy-models/releases/download"
+__download_url__ = "https://artifactory.riotinto.com/artifactory/spacy-models"
 __compatibility__ = "https://raw.githubusercontent.com/explosion/spacy-models/master/compatibility.json"
